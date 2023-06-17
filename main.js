2c10144d2d323c06901ca94a9b7a089734e32843
L4im4 <113398829+L4im4@users.noreply.github.com>
2023-06-17 11:51:33 -0600

    Ultima parte ya modificada
    Si mueves la nariz de izquierda a derecha se modifica la velocidad y si mueves la nariz de arriba hacia bajo se modifica el volumen.

--- main.js
+++ main.js
@@ -6,6 +6,9 @@ var leftx
 var lefty
 var rightx
 var righty
+var nosex
+var nosey
+
 
 function preload(){
 cancion=loadSound("Peaches.mp3")
@@ -26,6 +29,9 @@ leftx=res[0].pose.leftWrist.x
 lefty=res[0].pose.leftWrist.y
 rightx=res[0].pose.rightWrist.x
 righty=res[0].pose.rightWrist.y
+nosex=res[0].pose.nose.x
+nosey=res[0].pose.nose.y
+
 }
 
 function model(){
@@ -40,31 +46,49 @@ circle(leftx, lefty, 20)
 fill("green")
 circle(rightx, righty, 20)
 
-if (lefty >0 && lefty <100) {
+if (nosey >0 && nosey <100) {
 document.getElementById("volumen").innerHTML="El volumen es de 1"
 cancion.setVolume(1)
 }
-else if(lefty >100 && lefty <200){
+else if(nosey >100 && nosey <200){
     document.getElementById("volumen").innerHTML="El volumen es de 0.8"
     cancion.setVolume(0.8)
 }
-else if(lefty >200 && lefty <300){
+else if(nosey >200 && nosey <300){
     document.getElementById("volumen").innerHTML="El volumen es de 0.6"
     cancion.setVolume(0.6)
 }
-else if(lefty >300 && lefty <400){
+else if(nosey >300 && nosey <400){
     document.getElementById("volumen").innerHTML="El volumen es de 0.4"
     cancion.setVolume(0.4)
 }
-else if(lefty >400 && lefty <500){
+else if(nosey >400 && nosey <500){
     document.getElementById("volumen").innerHTML="El volumen es de 0.2"
     cancion.setVolume(0.2)
 }
 
-
-
-
+if (nosex >0 && nosex <100) {
+    document.getElementById("velocidad").innerHTML="La velocidad es de 0.5"
+    cancion.rate(0.5)
 }
+else if( nosex >100 && nosex <200){
+document.getElementById("velocidad").innerHTML="La velocidad es de 1"
+cancion.rate(1)
+}
+else if( nosex >200 && nosex <300){
+    document.getElementById("velocidad").innerHTML="La velocidad es de 1.5"
+    cancion.rate(1.5)
+    }
+    else if( nosex >300 && nosex <400){
+        document.getElementById("velocidad").innerHTML="La velocidad es de 2"
+        cancion.rate(2)
+        }
+        else if( nosex >400 && nosex <500){
+            document.getElementById("velocidad").innerHTML="La velocidad es de 2.5"
+            cancion.rate(2.5)
+            }
+}
+
 
 function start(){
     cancion.play()
